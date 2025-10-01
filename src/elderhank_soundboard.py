9620f297824fa3cb7e29c4c5486b40d6032097f2
Henry Allen <henry@allenhub.net>
2025-10-01 13:02:47 -0400

    Restructure GUI and adjust background color

--- src/elderhank_soundboard.py
+++ src/elderhank_soundboard.py
@@ -18,12 +18,20 @@ def stop_sound():
 root = tkinter.Tk()
 root.title("ElderHank Soundboard")
 root.geometry("400x200")
+root.configure(bg="#1f2f16")
 
 transport_bar = tkinter.Frame(root)
 transport_bar.pack(side=tkinter.BOTTOM, fill=tkinter.X, pady=10)
 
-play_button = tkinter.Button(transport_bar, text="Play Ambience", command=lambda: play_sound('../data/music/skyrim_awake.wav'))
-play_button.pack(side=tkinter.LEFT, padx=5, pady=5, expand=True)
+play_frame = tkinter.Frame(transport_bar)
+play_frame.pack(side=tkinter.LEFT, padx=5, pady=5, expand=True)
+
+# label creation
+test_label = tkinter.Label(play_frame, text="Active")
+test_label.pack(side=tkinter.TOP, pady=5)
+
+play_button = tkinter.Button(play_frame, text="Play Ambience", command=lambda: play_sound('../data/music/skyrim_awake.wav'))
+play_button.pack(side=tkinter.BOTTOM, padx=5, pady=5, expand=True)
 
 stop_button = tkinter.Button(transport_bar, text="Stop", command=stop_sound)
 stop_button.pack(side=tkinter.RIGHT, padx=5, pady=5, expand=True)
