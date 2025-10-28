5fca42113a681dd85dd37d4e83c351b977024d0e
Henry Allen <henry@allenhub.net>
2025-10-28 13:56:02 -0400

    Convert to customtkinter

--- src/elderhank_soundboard.py
+++ src/elderhank_soundboard.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import customtkinter as ctk
 from pygame import mixer
 
 mixer.init()
@@ -33,42 +34,42 @@ class MusicTrack:
 music = MusicTrack()
 
 #==========UI==========#
-root = tk.Tk()
+root = ctk.CTk()
 root.title("ElderHank Soundboard")
 root.geometry("400x200")
 
 #~~~TRANSPORT BAR~~~#
-transport_bar = tk.Frame(root, highlightthickness=3, highlightbackground="red")
-transport_bar.pack(side=tk.BOTTOM, fill=tk.X, pady=10)
+transport_bar = ctk.CTkFrame(root)
+transport_bar.pack(side=ctk.BOTTOM, fill=ctk.X, pady=10)
 
-stop_button = tk.Button(transport_bar, text="Stop", command=lambda: music.stop())
-stop_button.pack(side=tk.RIGHT, padx=5, pady=5, expand=True)
+stop_button = ctk.CTkButton(transport_bar, text="Stop", command=lambda: music.stop())
+stop_button.pack(side=ctk.RIGHT, padx=5, pady=5, expand=True)
 
-play_frame = tk.Frame(transport_bar, highlightthickness=3, highlightbackground="red")
-play_frame.pack(side=tk.LEFT, padx=5, pady=5, expand=True)
+play_frame = ctk.CTkFrame(transport_bar)
+play_frame.pack(side=ctk.LEFT, padx=5, pady=5, expand=True)
 
-play_button = tk.Button(play_frame, text="Play Ambience", command=lambda: music.play('./data/music/skyrim_awake.wav'))
-play_button.pack(side=tk.BOTTOM, padx=5, pady=5, expand=True)
+play_button = ctk.CTkButton(play_frame, text="Play Ambience", command=lambda: music.play('./data/music/skyrim_awake.wav'))
+play_button.pack(side=ctk.BOTTOM, padx=5, pady=5, expand=True)
 
 #~~~MUSIC TRACK~~~#
-music_track = tk.Frame(root, highlightthickness=3, highlightbackground="red")
-music_track.pack(side=tk.LEFT, padx=5, pady=5, expand=True)
+music_track = ctk.CTkFrame(root)
+music_track.pack(side=ctk.LEFT, padx=5, pady=5, expand=True)
 
-music_volume_label = tk.Label(music_track, text="100%")
-music_volume_label.pack(side=tk.BOTTOM, pady=5)
+music_volume_label = ctk.CTkLabel(music_track, text="100%")
+music_volume_label.pack(side=ctk.BOTTOM, pady=5)
 
 def set_music_volume(new_volume):
     """
     
     """
-    numeric_volume = float(new_volume)
-    music_volume_label.config(text=f"{numeric_volume}%")
+    numeric_volume = round(float(new_volume))
+    music_volume_label.configure(text=f"{numeric_volume}%")
     music.set_volume(numeric_volume * 0.01)
 
 
-music_volume = tk.Scale(music_track, from_=100, to=0, command=set_music_volume)
+music_volume = ctk.CTkSlider(music_track, from_=100, to=0, command=set_music_volume)
 music_volume.set(100)
-music_volume.pack(fill=tk.Y, padx=5, pady=5)
+music_volume.pack(fill=ctk.Y, padx=5, pady=5)
 
 
 root.mainloop()
