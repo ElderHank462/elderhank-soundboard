4f428d861f1cc855bf3f551256474699c4bddbed
Henry Allen <henry@allenhub.net>
2025-10-27 21:49:34 -0400

    Add music track volume functionality

--- src/elderhank_soundboard.py
+++ src/elderhank_soundboard.py
@@ -1,38 +1,74 @@
-import tkinter
+import tkinter as tk
 from pygame import mixer
 
 mixer.init()
+mixer.set_num_channels(2)
 
-def play_sound(sound_file):
-    """Plays a specified sound file."""
-    try:
-        mixer.music.load(sound_file)
-        mixer.music.play()
-    except Exception as e:
-        print(f"Error playing sound: {e}")
+class MusicTrack:
+    """
+    
+    """
+    def __init__(self):
+        pass
 
-def stop_sound():
-    """Stops the currently playing sound."""
-    mixer.music.stop()
+    def play(self, sound_file):
+        """Plays a specified sound file."""
+        try:
+            mixer.music.load(sound_file)
+            mixer.music.play()
+        except Exception as e:
+            print(f"Error playing sound: {e}")
 
-root = tkinter.Tk()
+    def stop(self):
+        """Stops the currently playing sound."""
+        mixer.music.stop()
+
+    def set_volume(self, new_volume):
+        """
+
+        """
+        float_volume = float(new_volume)
+        mixer.music.set_volume(float_volume)
+
+music = MusicTrack()
+
+#==========UI==========#
+root = tk.Tk()
 root.title("ElderHank Soundboard")
 root.geometry("400x200")
 
-transport_bar = tkinter.Frame(root)
-transport_bar.pack(side=tkinter.BOTTOM, fill=tkinter.X, pady=10)
+#~~~TRANSPORT BAR~~~#
+transport_bar = tk.Frame(root, highlightthickness=3, highlightbackground="red")
+transport_bar.pack(side=tk.BOTTOM, fill=tk.X, pady=10)
 
-play_frame = tkinter.Frame(transport_bar)
-play_frame.pack(side=tkinter.LEFT, padx=5, pady=5, expand=True)
+stop_button = tk.Button(transport_bar, text="Stop", command=lambda: music.stop())
+stop_button.pack(side=tk.RIGHT, padx=5, pady=5, expand=True)
 
-# label creation
-test_label = tkinter.Label(play_frame, text="Active")
-test_label.pack(side=tkinter.TOP, pady=5)
+play_frame = tk.Frame(transport_bar, highlightthickness=3, highlightbackground="red")
+play_frame.pack(side=tk.LEFT, padx=5, pady=5, expand=True)
 
-play_button = tkinter.Button(play_frame, text="Play Ambience", command=lambda: play_sound('../data/music/skyrim_awake.wav'))
-play_button.pack(side=tkinter.BOTTOM, padx=5, pady=5, expand=True)
+play_button = tk.Button(play_frame, text="Play Ambience", command=lambda: music.play('./data/music/skyrim_awake.wav'))
+play_button.pack(side=tk.BOTTOM, padx=5, pady=5, expand=True)
 
-stop_button = tkinter.Button(transport_bar, text="Stop", command=stop_sound)
-stop_button.pack(side=tkinter.RIGHT, padx=5, pady=5, expand=True)
+#~~~MUSIC TRACK~~~#
+music_track = tk.Frame(root, highlightthickness=3, highlightbackground="red")
+music_track.pack(side=tk.LEFT, padx=5, pady=5, expand=True)
+
+music_volume_label = tk.Label(music_track, text="100%")
+music_volume_label.pack(side=tk.BOTTOM, pady=5)
+
+def set_music_volume(new_volume):
+    """
+    
+    """
+    numeric_volume = float(new_volume)
+    music_volume_label.config(text=f"{numeric_volume}%")
+    music.set_volume(numeric_volume * 0.01)
+
+
+music_volume = tk.Scale(music_track, from_=100, to=0, command=set_music_volume)
+music_volume.set(100)
+music_volume.pack(fill=tk.Y, padx=5, pady=5)
+
 
 root.mainloop()
