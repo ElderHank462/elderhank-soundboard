4f8f42679013318201a9b35a7d67e9cf2707bce4
Henry Allen <henry@allenhub.net>
2025-10-28 17:35:10 -0400

    Restructure gui, create factory method for transport controls

--- src/elderhank_soundboard.py
+++ src/elderhank_soundboard.py
@@ -1,10 +1,25 @@
 import tkinter as tk
 import customtkinter as ctk
 from pygame import mixer
+import math
 
 mixer.init()
 mixer.set_num_channels(2)
 
+#========== STYLES ==========#
+DEFAULT_FONT = ("CTkFont", 24, "normal")
+
+
+BACKGROUND_COLOR = "#30342E"
+FRAME_COLOR = "#424640"
+PLAY_BUTTON_COLOR = "#395B50"
+PLAY_BUTTON_HOVER_COLOR = "#2a443c"
+STOP_BUTTON_COLOR = "#5A7684"
+STOP_BUTTON_HOVER_COLOR = "#40545F"
+TEXT_COLOR = "#dcdacd"
+
+
+#========== PYTHON COMPONENTS ==========#
 class MusicTrack:
     """
     
@@ -33,27 +48,57 @@ class MusicTrack:
 
 music = MusicTrack()
 
-#==========UI==========#
-root = ctk.CTk()
+
+#========== UI FACTORY FUNCTIONS ==========#
+def create_transport_button(parent, text, command, color, hover_color, scale_factor=1):
+    font_tuple = (DEFAULT_FONT[0], DEFAULT_FONT[1] * scale_factor, DEFAULT_FONT[2])
+    
+    button = ctk.CTkButton(
+        parent, 
+        width=scale_factor * 100, 
+        height=scale_factor * 80, 
+        text=text, 
+        command=command, 
+        fg_color=color, 
+        text_color=TEXT_COLOR, 
+        hover_color=hover_color,
+        font=font_tuple
+    )
+    return button
+
+
+#========== UI CONSTRUCTION ==========#
+root = ctk.CTk(fg_color=BACKGROUND_COLOR)
 root.title("ElderHank Soundboard")
-root.geometry("400x200")
+root.geometry("800x400")
 
-#~~~TRANSPORT BAR~~~#
-transport_bar = ctk.CTkFrame(root)
-transport_bar.pack(side=ctk.BOTTOM, fill=ctk.X, pady=10)
+#~~~GLOBAL TRANSPORT BAR~~~#
+transport_bar = ctk.CTkFrame(root, width=400, fg_color=FRAME_COLOR)
+transport_bar.pack(side=ctk.BOTTOM, expand=True)
 
-stop_button = ctk.CTkButton(transport_bar, text="Stop", command=lambda: music.stop())
+transport_bar_label = ctk.CTkLabel(transport_bar, text="Global Controls")
+transport_bar_label.pack(side=ctk.TOP, pady=5)
+
+stop_button = create_transport_button(transport_bar, "Stop", lambda: mixer.stop(), STOP_BUTTON_COLOR, STOP_BUTTON_HOVER_COLOR)
 stop_button.pack(side=ctk.RIGHT, padx=5, pady=5, expand=True)
 
-play_frame = ctk.CTkFrame(transport_bar)
-play_frame.pack(side=ctk.LEFT, padx=5, pady=5, expand=True)
-
-play_button = ctk.CTkButton(play_frame, text="Play Ambience", command=lambda: music.play('./data/music/skyrim_awake.wav'))
-play_button.pack(side=ctk.BOTTOM, padx=5, pady=5, expand=True)
+# TODO: Make global play button start both music and ambience channels
+play_button = create_transport_button(transport_bar, "Play", lambda: music.play('./data/music/skyrim_awake.wav'), PLAY_BUTTON_COLOR, PLAY_BUTTON_HOVER_COLOR)
+play_button.pack(side=ctk.LEFT, padx=5, pady=5, expand=True)
 
 #~~~MUSIC TRACK~~~#
-music_track = ctk.CTkFrame(root)
+music_track = ctk.CTkFrame(root, fg_color=FRAME_COLOR)
 music_track.pack(side=ctk.LEFT, padx=5, pady=5, expand=True)
+
+music_transport_bar = ctk.CTkFrame(music_track, fg_color=FRAME_COLOR)
+music_transport_bar.pack(side=ctk.BOTTOM, fill=ctk.X)
+
+music_stop_button = create_transport_button(music_transport_bar, "Stop", lambda: music.stop(), STOP_BUTTON_COLOR, STOP_BUTTON_HOVER_COLOR, 0.6)
+music_stop_button.pack(side=ctk.RIGHT, padx=5, pady=5, expand=True)
+
+music_play_button = create_transport_button(music_transport_bar, "Play", lambda: music.play('./data/music/skyrim_awake.wav'), PLAY_BUTTON_COLOR, PLAY_BUTTON_HOVER_COLOR, 0.6)
+music_play_button.pack(side=ctk.BOTTOM, padx=5, pady=5, expand=True)
+
 
 music_volume_label = ctk.CTkLabel(music_track, text="100%")
 music_volume_label.pack(side=ctk.BOTTOM, pady=5)
@@ -66,8 +111,7 @@ def set_music_volume(new_volume):
     music_volume_label.configure(text=f"{numeric_volume}%")
     music.set_volume(numeric_volume * 0.01)
 
-
-music_volume = ctk.CTkSlider(music_track, from_=100, to=0, command=set_music_volume)
+music_volume = ctk.CTkSlider(music_track, from_=0, to=100, command=set_music_volume, orientation=ctk.VERTICAL)
 music_volume.set(100)
 music_volume.pack(fill=ctk.Y, padx=5, pady=5)
 
