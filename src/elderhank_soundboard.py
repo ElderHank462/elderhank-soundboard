ed79f8ceb3bcbf8f773977fc765f990a8dba7d93
Henry Allen <henry@allenhub.net>
2025-11-26 17:47:49 -0500

    (Broken) Add ambience track

--- src/elderhank_soundboard.py
+++ src/elderhank_soundboard.py
@@ -95,6 +95,54 @@ class MusicTrack:
         self.music_volume_label.configure(text=f"{numeric_volume}%")
         mixer.music.set_volume(numeric_volume * 0.01)
 
+class AmbienceTrack:
+    """
+    The ambience track for the soundboard, containing the track UI and methods to control playback.
+    """
+    def __init__(self, master_widget):
+        self.master_widget = master_widget
+        self.ambience_track = ctk.CTkFrame(self.master_widget, fg_color=FRAME_COLOR)
+        self.ambience_track.pack(side=ctk.LEFT, padx=5, pady=5, expand=True)
+
+        self.ambience_transport_bar = TransportBar(self.ambience_track, "Ambience", lambda: self.play('./data/ambience/rain.mp3'), lambda: self.stop(), 0.6)
+        self.ambience_transport_bar.transport_bar.pack(side=ctk.BOTTOM, fill=ctk.X)
+
+
+        self.ambience_volume_label = ctk.CTkLabel(self.ambience_track, text="100%", text_color=TEXT_COLOR)
+        self.ambience_volume_label.pack(side=ctk.BOTTOM, pady=5)
+
+        self.ambience_volume = ctk.CTkSlider(self.ambience_track, from_=0, to=100, command=self.set_volume, orientation=ctk.VERTICAL)
+        self.ambience_volume.set(100)
+        self.ambience_volume.pack(fill=ctk.Y, padx=5, pady=5)
+
+
+    def play(self, sound_file):
+        """
+        Plays a specified sound file.
+        
+        Args:
+            sound_file: the path to a sound file
+        """
+        try:
+            ambience = mixer.Sound(sound_file)
+            ambience.play()
+        except Exception as e:
+            print(f"Error playing sound: {e}")
+
+    def stop(self):
+        """Stops the music that is playing currently."""
+        mixer.stop()
+
+    def set_volume(self, new_volume):
+        """
+        Set the volume of the music in the mixer and update the UI.
+
+        Args:
+            new_volume: the new volume to set
+        """
+        numeric_volume = round(float(new_volume))
+        self.ambience_volume_label.configure(text=f"{numeric_volume}%")
+        mixer.set_volume(numeric_volume * 0.01)
 
 #========== UI FACTORY FUNCTIONS ==========#
 def create_transport_button(parent, text, command, color, hover_color, scale_factor=1):
@@ -136,21 +184,14 @@ root.geometry("800x400")
 #~~~GLOBAL TRANSPORT BAR~~~#
 global_transport = TransportBar(root, "Global Controls", lambda: music_track.play('./data/music/skyrim_awake.wav'), lambda: stop_all())
 
-# transport_bar = ctk.CTkFrame(root, width=400, fg_color=FRAME_COLOR)
-# transport_bar.pack(side=ctk.BOTTOM, expand=True)
-
-# transport_bar_label = ctk.CTkLabel(transport_bar, text="Global Controls", text_color=TEXT_COLOR)
-# transport_bar_label.pack(side=ctk.TOP, pady=5)
-
-# stop_button = create_transport_button(transport_bar, "Stop", lambda: mixer.stop(), STOP_BUTTON_COLOR, STOP_BUTTON_HOVER_COLOR)
-# stop_button.pack(side=ctk.RIGHT, padx=5, pady=5, expand=True)
 
 # # TODO: Make global play button start both music and ambience channels
-# play_button = create_transport_button(transport_bar, "Play", lambda: music.play('./data/music/skyrim_awake.wav'), PLAY_BUTTON_COLOR, PLAY_BUTTON_HOVER_COLOR)
-# play_button.pack(side=ctk.LEFT, padx=5, pady=5, expand=True)
 
 #~~~MUSIC TRACK~~~#
 music_track = MusicTrack(root)
+
+#~~~AMBIENCE TRACK~~~#
+ambience_track = AmbienceTrack(root)
 
 
 
